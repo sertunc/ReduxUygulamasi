a7d33cc1f66dc6f3034d33685009fb52831ac036
Cahit Sertunç SELEN <cahitsertunc.selen@kaleyazilim.internal>
2018-11-22 17:33:31 +0300

    package.json güncellendi

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Ogrenciler from './components/Ogrenciler';
 import Siniflar from './components/Siniflar';
